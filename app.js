2cd3e930e2a4a7d7f511cb13b131856792ba1840
Znak Zorro <ZnakZorro@users.noreply.github.com>
2019-06-24 13:39:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,31 +5,28 @@ var fs = require('fs');
 
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+	fs.readFile(__dirname + '/index.html',
+	function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'info from server - world' });
-  socket.on('informacje', function (data) {
-    console.log(data);
-  });
-  
-			setInterval(function(){
-				var czas = (new Date()).toLocaleString();
-				socket.emit('news', { my: 'data',serverTime:czas });
-			},5000)  
-  
+socket.emit('news', { hello: 'info from server - world' });
+socket.on('informacje', function (data) {
+	console.log(data);
 });
 
+setInterval(function(){
+	var czas = (new Date()).toLocaleString();
+	socket.emit('news', { my: 'data',serverTime:czas });
+},5000)  
 
+});
 
 app.listen(8282,function(x){console.log(8282,x)});
